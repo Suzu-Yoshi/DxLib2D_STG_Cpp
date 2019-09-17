357120cf115423f6214a20aff77d8b5678a5d06d
Hmj_SuzuYoshi <acver.SunnyBox@gmail.com>
2019-09-17 18:14:49 +0900

    プロジェクト ファイルを追加します。

---
+++ FPS.hpp
@@ -0,0 +1,112 @@
+/*--+----1----+----2----+----3----+----4----+----5-----+----6----+----7----+----8----+----9----+---*/
+/* fps.h     																					   */
+/* FPS(Flame Per Second)クラス																	   */
+/* 一つのインスタンスのみにする																	   */
+
+#pragma once
+
+//########## ヘッダーファイル読み込み ##########
+#include "DxLib.h"
+
+//########## クラスの定義 ##########
+class FPS
+{
+private:
+	int			value;					//FPS値
+	double		drawValue;				//DrawするときのFPS値
+	DWORD		calcStartTime;			//0フレーム目の開始時刻
+	DWORD		calcEndTime;			//設定したフレームの終了時刻
+	DWORD		flameCount;				//フレームのカウント
+	double		calcAverage; 			//FPSを計算するための平均サンプル数
+
+public:
+
+	//コンストラクタ
+	//引　数：int　：FPS値
+	FPS(int fps_value)
+	{
+		this->value = fps_value;
+		this->drawValue = 0;
+		this->calcStartTime = 0;
+		this->calcEndTime = 0;
+		this->flameCount = 0;
+		this->calcAverage = (double)value;
+
+		return;
+	}
+
+	//指定したFPSになるように待つ
+	VOID Wait(VOID)
+	{
+		//現在の時刻をミリ秒で取得
+		DWORD nowTime = GetTickCount();
+
+		//1フレーム目から実際にかかった時間を計算
+		DWORD keikaTime = nowTime - this->calcStartTime;
+
+		//ゼロ除算対策
+		if (this->value > 0)
+		{
+			//待つべき時間 = フレーム数ごとにかかるべき時間 - 実際にかかった時間;
+			int wait_tm = (this->flameCount * 1000 / this->value) - (keikaTime);
+
+			//待つべき時間があった場合
+			if (wait_tm > 0)
+			{
+				//ミリ秒分、処理を中断する
+				Sleep(wait_tm);
+			}
+		}
+
+		return;
+	}
+
+	//画面更新の時刻を取得する
+	VOID Update(VOID)
+	{
+		//1フレーム目なら時刻を記憶
+		if (this->flameCount == 0)
+		{
+			//Windowsが起動してから現在までの時刻をミリ秒で取得
+			this->calcStartTime = GetTickCount();
+		}
+
+		//指定フレームの最後のフレームなら、平均を計算する
+		if (this->flameCount == this->calcAverage)
+		{
+			//Windowsが起動してから現在までの時刻をミリ秒で取得
+			this->calcEndTime = GetTickCount();
+
+			//平均的なFPS値を計算
+			this->drawValue = 1000.0f / ((this->calcEndTime - this->calcStartTime) / this->calcAverage);
+
+			//次のFPS計算の準備
+			this->calcStartTime = this->calcEndTime;
+
+			//カウント初期化
+			this->flameCount = 0;
+		}
+
+		//フレーム数カウントアップ
+		this->flameCount++;
+
+		return;
+	}
+
+	//FPSの値を表示する
+	VOID Draw(int drawX, int drawY)
+	{
+		//文字列を描画
+		DrawFormatString(drawX, drawY, GetColor(255, 255, 255), "FPS:%.1f", this->drawValue);
+		return;
+	}
+
+private:
+
+	//デストラクタ
+	~FPS()
+	{
+
+	}
+};
+
---
+++ main.cpp
@@ -0,0 +1,61 @@
+/*--+----1----+----2----+----3----+----4----+----5-----+----6----+----7----+----8----+----9----+---*/
+/* main.cpp     																				　 */
+/* C++でSTG																						　 */
+
+//##########ヘッダーファイル読み込み ##########
+#include "DxLib.h"
+#include "FPS.hpp"
+#include <math.h>
+
+//########## マクロ定義 ##########
+#define GAME_WIDTH	800		//画面の横の大きさ
+#define GAME_HEIGHT	600		//画面の縦の大きさ
+#define GAME_COLOR	32		//画面のカラービット
+
+#define GAME_WINDOW_NAME	"GAME TITLE"		//ウィンドウのタイトル
+#define GAME_WINDOW_MODECHANGE	TRUE			//TRUE：ウィンドウモード / FALSE：フルスクリーン
+
+#define SET_WINDOW_ST_MODE_DEFAULT			0	//デフォルト
+#define SET_WINDOW_ST_MODE_TITLE_NONE		1	//タイトルバーなし
+#define SET_WINDOW_ST_MODE_TITLE_FLAME_NONE	2	//タイトルバーとフレームなし
+#define SET_WINDOW_ST_MODE_FLAME_NONE		3	//フレームなし
+
+#define GAME_FPS_SPEED					   60
+
+//########## プログラムで最初に実行される関数 ##########
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(GAME_WINDOW_MODECHANGE);					//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);			//指定の数値で画面を表示する
+
+	SetWindowStyleMode(SET_WINDOW_ST_MODE_DEFAULT);				//タイトルバーあり
+
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));					//タイトルの文字
+
+	if (DxLib_Init() == -1) { return -1; }						//ＤＸライブラリ初期化処理
+
+	SetDrawScreen(DX_SCREEN_BACK);								//Draw系関数は裏画面に描画
+
+	FPS *fps = new FPS(GAME_FPS_SPEED);							//FPSクラスのオブジェクトを生成
+
+	while (TRUE)	//無限ループ
+	{
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
+
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+
+		//MY_ALL_KEYDOWN_UPDATE();				//キーの状態を取得
+
+		fps->Update();		//FPSの処理[更新]
+
+		fps->Draw(0,0);		//FPSの処理[描画]
+
+		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+
+		fps->Wait();		//FPSの処理[待つ]
+	}
+
+	DxLib_End();		//ＤＸライブラリ使用の終了処理
+
+	return 0;
+}
